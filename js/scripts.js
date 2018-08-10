10307406ee429619c5d23b334c8b8b5b5255d8fe
Mel Yasuda <yasudamel@gmail.com>
2018-08-10 15:22:06 -0700

    comment out js logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,24 +8,26 @@ $(document).ready(function() {
     var attitude = $("input:radio[name=attitude]:checked").val();
     var name = $("#name").val();
 
-    if (!design || !orgSize || !googleOrMs || !career || !attitude || !name) {
-      alert("Answer all the questions and fill out your name before you submit!")
-      } else if (design === "yes" && orgSize === "big" && attitude === "showoff") {
-        $("#result").text("React! You can build creative websites and show off to yours friends!");
-        $("#resultname").text(name);
-        $("#resultform").fadeIn("slow").show();
-      } else if (googleOrMs === "ms" && orgSize === "big" && attitude === "humble") {
-        $("#result").text("C#! Your works might not be the coolest but surely helps people in their daily lives!");
-        $("#resultname").text(name);
-        $("#resultform").fadeIn("slow").show();
-      } else if (career === "change" && googleOrMs === "google" && design === "no") {
-        $("#result").text("PHP! You will have options to work for different kinds of companies!");
-        $("#resultname").text(name);
-        $("#resultform").fadeIn("slow").show();
-      } else if (career === "change" && googleOrMs === "google" && design === "no") {
-        $("#result").text("JAVA! Android developpers are in short! They need your help!");
-        $("#resultname").text(name);
-        $("#resultform").fadeIn("slow").show();
-      }
+    alert((design).val());
+
+    // if (!design || !orgSize || !googleOrMs || !career || !attitude || !name) {
+    //   alert("Answer all the questions and fill out your name before you submit!")
+    //   } else if (design === "yes" && orgSize === "big" && attitude === "showoff") {
+    //     $("#result").text("React! You can build creative websites and show off to yours friends!");
+    //     $("#resultname").text(name);
+    //     $("#resultform").fadeIn("slow").show();
+    //   } else if (googleOrMs === "ms" && orgSize === "big" && attitude === "humble") {
+    //     $("#result").text("C#! Your works might not be the coolest but surely helps people in their daily lives!");
+    //     $("#resultname").text(name);
+    //     $("#resultform").fadeIn("slow").show();
+    //   } else if (career === "change" && googleOrMs === "google" && design === "no") {
+    //     $("#result").text("PHP! You will have options to work for different kinds of companies!");
+    //     $("#resultname").text(name);
+    //     $("#resultform").fadeIn("slow").show();
+    //   } else if (career === "change" && googleOrMs === "google" && design === "no") {
+    //     $("#result").text("JAVA! Android developpers are in short! They need your help!");
+    //     $("#resultname").text(name);
+    //     $("#resultform").fadeIn("slow").show();
+    //   }
   });
 });
