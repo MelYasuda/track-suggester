06afca8d568d4c525e44b2676cd899359a720249
Mel Yasuda <yasudamel@gmail.com>
2018-08-10 13:36:32 -0700

    fix camel case in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,30 @@ $(document).ready(function() {
   $("form#suggestion").submit(function(event) {
     event.preventDefault();
     var design = $("input:radio[name=design]:checked").val();
-    var orgsize = $("input:radio[name=orgsize]:checked").val();
-    var googleorms = $("input:radio[name=googleorms]:checked").val();
+    var orgSize = $("input:radio[name=orgsize]:checked").val();
+    var googleOrMs = $("input:radio[name=googleorms]:checked").val();
     var career = $("input:radio[name=career]:checked").val();
     var attitude = $("input:radio[name=attitude]:checked").val();
     var name = $("#name").val();
 
-    if (!design || !orgsize || !googleorms || !career || !attitude || !name) {
+    if (!design || !orgSize || !googleOrMs || !career || !attitude || !name) {
       alert("Answer all the questions and fill out your name before you submit!")
-      } else if (design === "yes" && orgsize === "big" && attitude === "showoff") {
+      } else if (design === "yes" && orgSize === "big" && attitude === "showoff") {
         $("#result").text("React! You can build creative websites and show off to yours friends!");
         $("#resultname").text(name);
-        $("#resultform").show();
-      } else if (googleorms === "ms" && orgsize === "big" && attitude === "humble") {
+        $("#resultform").fadeIn("slow").show();
+      } else if (googleOrMs === "ms" && orgSize === "big" && attitude === "humble") {
         $("#result").text("C#! Your works might not be the coolest but surely helps people in their daily lives!");
         $("#resultname").text(name);
-        $("#resultform").show();
-      } else if (career === "change" && googleorms === "google" && design === "no") {
+        $("#resultform").fadeIn("slow").show();
+      } else if (career === "change" && googleOrMs === "google" && design === "no") {
         $("#result").text("PHP! You will have options to work for different kinds of companies!");
         $("#resultname").text(name);
-        $("#resultform").show();
-      } else if (career === "change" && googleorms === "google" && design === "no") {
+        $("#resultform").fadeIn("slow").show();
+      } else if (career === "change" && googleOrMs === "google" && design === "no") {
         $("#result").text("JAVA! Android developpers are in short! They need your help!");
         $("#resultname").text(name);
-        $("#resultform").show();
+        $("#resultform").fadeIn("slow").show();
       }
   });
 });
